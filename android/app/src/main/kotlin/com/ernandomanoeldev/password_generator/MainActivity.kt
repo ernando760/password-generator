f857d09822334e6256f6ad3be5796163a350f4d2
ErnandoM <ernandomanoel.freire@gmail.com>
2024-03-08 11:12:53 -0300

    configurando o projeto

---
+++ android/app/src/main/kotlin/com/ernandomanoeldev/password_generator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ernandomanoeldev.password_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/shared/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const red = Color(0xFFDB222A);
+const white = Color(0xFFE1E1E2);
+const orange = Color(0xFFFFA400);
+const richBlack = Color(0xFF011C27);
+const green = Color(0xFF00CC66);
+const gray = Color(0xFF7C7C7C);
