225033efae68a26a1a6b06fcdf1e0df45fa372f4
ErnandoM <ernandomanoel.freire@gmail.com>
2024-03-15 11:36:14 -0300

    refatorando o codigo

--- lib/src/modules/home/screen/home_screen.dart
+++ lib/src/modules/home/screen/home_screen.dart
@@ -1,63 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:password_generator/src/modules/home/controller/home_controller.dart';
+import 'package:password_generator/src/modules/home/repository/home_repository.dart';
 
 import 'package:password_generator/src/modules/home/widgets/card_character_widget.dart';
 import 'package:password_generator/src/modules/home/widgets/cards_check_box_widget.dart';
+import 'package:password_generator/src/modules/services/local_storage.dart';
 
 import 'package:password_generator/src/shared/constant/colors.dart';
 import 'package:password_generator/src/shared/widgets/custom_app_bar_widget.dart';
-import 'package:password_generator/src/shared/widgets/custom_bottom_navigation_bar_widget.dart';
 import 'package:provider/provider.dart';
+
+final $homeProvider = Provider<HomeRepository>(
+    create: (context) => IHomeRepository(context.read<LocalStorage>()),
+    child: ChangeNotifierProvider(
+        create: (context) => HomeController(context.read<HomeRepository>()),
+        child: const HomeScreen()));
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => HomeController(),
-      child: Scaffold(
-        backgroundColor: richBlack,
-        body: SafeArea(
-            child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // app bar
-                const Flexible(
-                  child: CustomAppBarWidget(title: "Gerador de Senha"),
+    return Scaffold(
+      backgroundColor: richBlack,
+      body: SafeArea(
+          child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // app bar
+              const Flexible(
+                child: CustomAppBarWidget(title: "Gerador de Senha"),
+              ),
+              // image lock
+              const SizedBox(
+                height: 15,
+              ),
+              Flexible(
+                child: Image.asset(
+                  "assets/images/lock_black.png",
+                  fit: BoxFit.cover,
                 ),
-                // image lock
-                const SizedBox(
-                  height: 15,
-                ),
-                Flexible(
-                  child: Image.asset(
-                    "assets/images/lock_black.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // card_character
-                const Flexible(
-                  child: CardCharacterWidget(),
-                ),
-                const SizedBox(
-                  height: 28,
-                ),
-                // cards
-                const Flexible(
-                  child: CardsCheckBoxWidget(),
-                )
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // card_character
+              const Flexible(
+                child: CardCharacterWidget(),
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              // cards
+              const Flexible(
+                child: CardsCheckBoxWidget(),
+              )
+            ],
           ),
-        )),
-        bottomNavigationBar: const CustomBottomNavigationBarWidget(),
-      ),
+        ),
+      )),
+      // bottomNavigationBar: const CustomBottomNavigationBarWidget(),
     );
   }
 }
